70a07ac3349f73f08cfffb48e69dc156b1ec7d97
01AnshYadav <yadav01ansh2006@gmail.com>
2026-08-21 00:35:33 +0530

    Add Supabase database schema and migrations

---
+++ supabase/migrations/20260820185507_remote_schema.sql
@@ -0,0 +1,6 @@
+alter default privileges for role "postgres" in schema "public" revoke all on sequences from "anon";
+
+alter default privileges for role "postgres" in schema "public" revoke all on sequences from "authenticated";
+
+alter default privileges for role "postgres" in schema "public" revoke all on sequences from "service_role";
+
